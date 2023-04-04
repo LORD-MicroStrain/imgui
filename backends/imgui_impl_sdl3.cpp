b330ffcfe3d735e99390ca3140aee20413c0efb7
microstrain-sam <105877710+microstrain-sam@users.noreply.github.com>
2023-04-04 15:44:22 -0400

    Disable SDL_MouseCapture on Linux for debug builds so that the mouse works when stopping on breakpoints.

--- backends/imgui_impl_sdl3.cpp
+++ backends/imgui_impl_sdl3.cpp
@@ -500,8 +500,11 @@ static void ImGui_ImplSDL3_UpdateMouseData()
 
     // We forward mouse input when hovered or captured (via SDL_EVENT_MOUSE_MOTION) or when focused (below)
 #if SDL_HAS_CAPTURE_AND_GLOBAL_MOUSE
+#if !defined(_WIN32) && defined(NDEBUG)
     // SDL_CaptureMouse() let the OS know e.g. that our imgui drag outside the SDL window boundaries shouldn't e.g. trigger other operations outside
+    // On linux, this can get the mouse stuck when a breakpoint is hit, so disable for debug builds.
     SDL_CaptureMouse((bd->MouseButtonsDown != 0) ? SDL_TRUE : SDL_FALSE);
+#endif
     SDL_Window* focused_window = SDL_GetKeyboardFocus();
     const bool is_app_focused = (focused_window && (bd->Window == focused_window || ImGui::FindViewportByPlatformHandle((void*)focused_window)));
 #else
